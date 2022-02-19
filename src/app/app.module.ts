b37b95c050c53523617a3094f2dc643d8e660c09
chotr <minhphungbmt141999@gmail.com>
2022-02-19 11:16:44 +0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './clients/navbar/navbar.component';
+import { CarouselComponent } from './clients/carousel/carousel.component';
+import { AboutComponent } from './clients/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
