e1388a7b5da6c86d7b999f698ab907fc28dd54e4
chotr <minhphungbmt141999@gmail.com>
2022-02-21 23:33:52 +0700

    responsive 3 part

--- src/app/clients/carousel/carousel.component.ts
+++ src/app/clients/carousel/carousel.component.ts
@@ -3,15 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
+  number = 60;
+  width: number;
+  constructor() {}
 
   ngOnInit(): void {
+    this.width = window.innerWidth
+    if(this.width <= 1199){
+      this.number = 50
+    }
+    if(this.width <= 767.89){
+      this.number = 40
+    }
+    if(this.width <= 570){
+      this.number = 30
+    }
   }
   numSequence(n: number): Array<number> {
     return Array(n);
   }
+
 }
